55158c546b0d05d4588ca63c3637a088d220573b
karachunvladislav <karachunvladislav@gmail.com>
2021-03-05 13:50:56 +0300

    fix: icon SquareIconBtn

--- src/SquareIconBtn.stories.tsx
+++ src/SquareIconBtn.stories.tsx
@@ -6,7 +6,7 @@ import { MetSquareIconBtnProps } from "../components/SquareIconBtn";
 import { action } from "@storybook/addon-actions";
 import { withKnobs, color, text } from "@storybook/addon-knobs";
 
-import starIcon from "./assets/icons/star-icon.js";
+import trashIcon from "./assets/icons/trash-icon.js";
 
 export default {
   title: "Example/SquareIconBtn",
@@ -21,7 +21,7 @@ export const Default = () => {
     hoverColor: color("Hover color", undefined),
     defaultIconColor: color("Default icon color", undefined),
     hoverIconColor: color("Hover icon color", undefined),
-    icon: starIcon,
+    icon: trashIcon,
   });
 
   return <SquareIconBtn {...SquareIconBtnProps()} />;
