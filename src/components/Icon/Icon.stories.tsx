f0faa0daf0d933f47bcc64511775d122eae6f017
Kristina-Isakovich <kristinaisakovich0621@gmail.com>
2021-09-01 14:33:23 +0300

    feat: added Labels

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,12 +1,53 @@
 import React from "react";
 
+import styled, { css } from "styled-components";
+
 import { Meta } from "@storybook/react/types-6-0";
 
-import MetIcon, { MetIconProps } from "./index";
+import MetIcon, { MetIconProps, icons } from "./index";
 
 import { withKnobs, color, text, number } from "@storybook/addon-knobs";
-
 import mdx from "./Icon.mdx";
+
+const Name = styled.div`
+    color: #666;
+    font-size: 12px;
+`;
+
+const Item = styled.li<{ minimal?: boolean }>`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  flex: 0 1 20%;
+  min-width: 200px;
+  padding: 0 7.5px 20px;
+  svg {
+    margin-right: 10px;
+    width: 30px;
+    height: 30px;
+  }
+  ${(props) =>
+  props.minimal &&
+  css`
+      flex: none;
+      min-width: auto;
+      padding: 0;
+      background: #fff;
+      border: 1px solid #666;
+      svg {
+        display: block;
+        margin-right: 0;
+        width: 48px;
+        height: 48px;
+      }
+    `};
+`;
+
+const List = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  list-style: none;
+`;
 
 export default {
     title: "Example/Icon",
@@ -29,3 +70,17 @@ export const Default = () => {
 
     return <MetIcon {...IconProps()} />;
 };
+
+export const Labels = () => (
+  <>
+      There are {Object.keys(icons).length} icons
+      <List>
+          {Object.keys(icons).map((key) => (
+            <Item key={key}>
+                <MetIcon name={key as keyof typeof icons} />
+                <Name>{key}</Name>
+            </Item>
+          ))}
+      </List>
+  </>
+);
--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,6 +1,7 @@
 import { MetIcon } from "./Icon";
 import { MetIconProps } from "./IconProps";
+import { icons } from "./icons";
 
-export { MetIconProps };
+export { MetIconProps, icons };
 
 export default MetIcon;
