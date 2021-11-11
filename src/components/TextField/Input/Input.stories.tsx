a964f288b6413d1beae1957a1dde0d357fff2295
Kristina-Isakovich <kristinaisakovich0621@gmail.com>
2021-11-11 18:23:28 +0300

    feat: added labelChildren in TextField components

--- src/components/TextField/Input/Input.stories.tsx
+++ src/components/TextField/Input/Input.stories.tsx
@@ -3,64 +3,64 @@ import React, { useState } from "react";
 import { Meta } from "@storybook/react/types-6-0";
 
 import MetTextField, {
-    MetInputProps,
-    TextFieldState as stateTF,
+  MetInputProps,
+  TextFieldState as stateTF,
 } from "./index";
 
 import {
-    withKnobs,
-    boolean,
-    color,
-    text,
-    select,
+  withKnobs,
+  boolean,
+  color,
+  text,
+  select,
 } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 
 import mdx from "./Input.mdx";
 
 export default {
-    title: "Example/TextField",
-    component: MetTextField,
-    decorators: [withKnobs],
-    parameters: {
-        docs: {
-            page: mdx,
-        },
+  title: "Example/TextField",
+  component: MetTextField,
+  decorators: [withKnobs],
+  parameters: {
+    docs: {
+      page: mdx,
     },
+  },
 } as Meta;
 
 export const Default = () => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const onChange = (event) => {
-        setValue(event.target.value);
-    };
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    const TextFieldProps = (): MetInputProps => ({
-        value: value,
-        onChange: onChange,
-        onBlur: action("onBlur"),
-        isTextSelected: boolean("Is the text focused in input", false),
-        isTextField: boolean("Is component input/textField", false),
-        label: text("Label when component text field", "Label"),
-        isDisabled: boolean("Is component disabled", false),
-        state: select(
-            "State",
-            {
-                Default: stateTF.DEFAULT,
-                Error: stateTF.ERROR,
-                Success: stateTF.SUCCESS,
-            },
-            stateTF.DEFAULT
-        ),
-        hoverColor: color("Hover color", undefined),
-        focusColor: color("Focus color", undefined),
+  const TextFieldProps = (): MetInputProps => ({
+    value: value,
+    onChange: onChange,
+    onBlur: action("onBlur"),
+    isTextSelected: boolean("Is the text focused in input", false),
+    isTextField: boolean("Is component input/textField", false),
+    label: text("Label when component text field", "Label"),
+    isDisabled: boolean("Is component disabled", false),
+    state: select(
+      "State",
+      {
+        Default: stateTF.DEFAULT,
+        Error: stateTF.ERROR,
+        Success: stateTF.SUCCESS,
+      },
+      stateTF.DEFAULT
+    ),
+    hoverColor: color("Hover color", undefined),
+    focusColor: color("Focus color", undefined),
 
-        errorColor: color("Error color", undefined),
-        errorMessage: text("Error message", "Error message"),
-        successColor: color("Success color", undefined),
-        successMessage: text("Success message", "Success message"),
-    });
+    errorColor: color("Error color", undefined),
+    errorMessage: text("Error message", "Error message"),
+    successColor: color("Success color", undefined),
+    successMessage: text("Success message", "Success message"),
+  });
 
-    return <MetTextField {...TextFieldProps()} />;
+  return <MetTextField {...TextFieldProps()} />;
 };
--- src/components/TextField/TextField.interface.ts
+++ src/components/TextField/TextField.interface.ts
@@ -1,76 +1,80 @@
 import { TextFieldState as stateTF } from "./textFieldState.enum";
 
 export interface MetTextFieldProps {
-    /**
-     * Additional component styles
-     */
-    style?: object;
-    /**
-     * Additional component styles
-     */
-    className?: string;
-    /**
-     * Input font for component
-     */
-    inputFontClass?: string;
-    /**
-     * Label font for component
-     */
-    labelFontClass?: string;
-    /**
-     * Value
-     */
-    value: string;
-    /**
-     * Function for trigger event onChange
-     */
-    onChange: (event: any) => void;
-    /**
-     * Function for trigger event onBlur
-     */
-    onBlur?: (event: any) => void;
-    /**
-     * Is the text selected in text field
-     */
-    isTextSelected?: boolean;
-    /**
-     * Placeholder
-     */
-    placeholder?: string;
-    /**
-     * Is component disabled
-     */
-    isDisabled?: boolean;
-    /**
-     * Set visual type of component TextField/Input
-     */
-    isTextField: boolean;
-    /**
-     * Set label for TextField component
-     */
-    label?: string;
-    /**
-     * Set component state
-     */
-    state?: stateTF;
-    /**
-     * Сhange border color when input is default
-     */
-    defaultColor?: string;
-    /**
-     * Сhange border color when input is hovered
-     */
-    hoverColor?: string;
-    /**
-     * Сhange border color when input is focused
-     */
-    focusColor?: string;
-    /**
-     * Сhange color of border/icon/message when input is error
-     */
-    errorColor?: string;
-    /**
-     * Сhange color of border/icon/message when input is success
-     */
-    successColor?: string;
+  /**
+   * Additional component styles
+   */
+  style?: object;
+  /**
+   * Additional component styles
+   */
+  className?: string;
+  /**
+   * Input font for component
+   */
+  inputFontClass?: string;
+  /**
+   * Label font for component
+   */
+  labelFontClass?: string;
+  /**
+   * Value
+   */
+  value: string;
+  /**
+   * Function for trigger event onChange
+   */
+  onChange: (event: any) => void;
+  /**
+   * Function for trigger event onBlur
+   */
+  onBlur?: (event: any) => void;
+  /**
+   * Is the text selected in text field
+   */
+  isTextSelected?: boolean;
+  /**
+   * Placeholder
+   */
+  placeholder?: string;
+  /**
+   * Is component disabled
+   */
+  isDisabled?: boolean;
+  /**
+   * Set visual type of component TextField/Input
+   */
+  isTextField: boolean;
+  /**
+   * Set label for TextField component
+   */
+  label?: string;
+  /**
+   * The element that comes after label, or instead of him.
+   */
+  labelChildren?: React.ReactNode;
+  /**
+   * Set component state
+   */
+  state?: stateTF;
+  /**
+   * Сhange border color when input is default
+   */
+  defaultColor?: string;
+  /**
+   * Сhange border color when input is hovered
+   */
+  hoverColor?: string;
+  /**
+   * Сhange border color when input is focused
+   */
+  focusColor?: string;
+  /**
+   * Сhange color of border/icon/message when input is error
+   */
+  errorColor?: string;
+  /**
+   * Сhange color of border/icon/message when input is success
+   */
+  successColor?: string;
 }
