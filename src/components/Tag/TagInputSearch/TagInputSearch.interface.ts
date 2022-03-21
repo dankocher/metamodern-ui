4e428d8e4e4fb9f1631903b31852bdf366264b33
ivan <severinec7788@mail.ru>
2022-03-21 13:40:02 +0300

    feat: created a new component TagInputSearch

---
+++ src/components/Tag/TagInputSearch/TagInputSearch.interface.ts
@@ -0,0 +1,37 @@
+import { MetTagProps } from "../Tag.interface";
+import { MetCircleIconBtnProps } from "../../CircleIconBtn";
+
+export interface MetTagInputSearchProps extends MetTagProps, MetCircleIconBtnProps {
+    /**
+     * Font color when component is hovered
+     */
+    hoverFontColor?: string;
+    /**
+     * Function for trigger event onChange
+     */
+    onChange: (event: any) => void;
+    /**
+     * Function for trigger event onBlur
+     */
+    onBlur?: (event: any) => void;
+    /**
+     * Function for trigger when clicked checkbox
+     */
+    onToggle?: () => void;
+    /**
+     * Set state when component is checked/unchecked
+     */
+    isChecked: boolean;
+    /**
+     * Substring to highlight
+     */
+    searchValue?: string;
+    /**
+     * Сhange hover checkbox color
+     */
+    hoverCheckboxColor?: string;
+    /**
+     * Add ref to input
+     */
+    innerRef?: any;
+}
---
+++ src/components/Tag/TagInputSearch/TagInputSearch.tsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./index.module.scss";
+
+import styled from "styled-components";
+
+import { colors } from "../../styles/colors.js";
+
+import { MetTagInputSearchProps } from "./index";
+
+import MetCircleIconBtn, {
+  SizeCircleIconBtn as Size,
+} from "../../CircleIconBtn";
+import MetIcon, { Icons } from "../../Icon";
+
+const classNames = require("classnames");
+
+const Container = styled.div`
+  border-color: ${({ isFocused, defaultColor, focusColor }) =>
+    isFocused ? focusColor : defaultColor};
+  .${styles.textWrapper} {
+    color: ${({ isFocused, defaultColor, hoverFontColor }) =>
+      isFocused ? hoverFontColor : defaultColor};
+  }
+  :hover {
+    .${styles.textWrapper} {
+      color: ${({ hoverFontColor }) => hoverFontColor};
+    }
+  }
+  .${styles.container__checkbox}:hover {
+    .${styles.container__checkbox__hover} {
+      background-color: ${({ hoverCheckboxColor }) => hoverCheckboxColor};
+    }
+  }
+`;
+
+export const MetTagInputSearch: React.FC<MetTagInputSearchProps> = ({
+  style,
+  className = "",
+  fontClass = styles.body1,
+
+  hoverFontColor = colors.neutral700,
+  hoverCheckboxColor = colors.neutral200,
+  defaultColor = colors.neutral600,
+  focusColor = colors.blue,
+  checkedIcon = Icons.starFilled,
+  uncheckedIcon = Icons.starOutlined,
+  onChange,
+  onBlur,
+  isHasCheckbox = true,
+  onToggle,
+  isChecked,
+  value,
+  searchValue,
+  innerRef,
+
+  onClick,
+  icon,
+  bgColor,
+  defaultIconColor,
+  hoverColor,
+  styleIconBtn,
+  classNameIconBtn,
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const toggle = () => {
+    onToggle && onToggle();
+  };
+
+  const onBlurHandler = (event) => {
+    setIsFocused(false);
+
+    onBlur && onBlur(event);
+  };
+
+  const openInput = (e) => {
+    if (e.target.lastElementChild == null) return;
+    e.target.lastElementChild.select();
+  };
+
+  const focusHandel = (e) => {
+    setIsFocused(true);
+    e.target.select();
+  };
+
+  const stateStyle = classNames(styles.container, {
+    [styles.focused__withCB]: isFocused && isHasCheckbox,
+    [styles.focused__withoutCB]: isFocused && !isHasCheckbox,
+    [styles.unfocused__withoutCB]: !isFocused && !isHasCheckbox,
+  });
+
+  const [partsText, setPartsText] = useState({ head: null, teal: null });
+
+  useEffect(() => {
+    let index = value.indexOf(searchValue);
+    if (index === -1) setPartsText({ head: value, teal: null });
+    else
+      setPartsText({
+        head: value.slice(0, index),
+        teal: value.slice(index + searchValue.length, value.length),
+      });
+  }, [searchValue, value]);
+
+  return (
+    <Container
+      style={style}
+      className={`${stateStyle} ${className}`}
+      hoverFontColor={hoverFontColor}
+      defaultColor={defaultColor}
+      hoverCheckboxColor={hoverCheckboxColor}
+      focusColor={focusColor}
+      isFocused={isFocused}
+    >
+      {isHasCheckbox ? (
+        <div className={styles.container__checkbox} onClick={toggle}>
+          <div className={styles.container__checkbox__hover}>
+            {isChecked ? (
+              <MetIcon icon={checkedIcon} size={16} color={colors.accent1} />
+            ) : (
+              <MetIcon
+                icon={uncheckedIcon}
+                size={16}
+                color={colors.neutral300}
+              />
+            )}
+          </div>
+        </div>
+      ) : null}
+      <div className={styles.container__content} onClick={openInput}>
+        <div className={styles.textWrapper}>
+          <span className={fontClass}>
+            {partsText.head}
+            {partsText.teal !== null && (
+              <span className={styles.substring}>{searchValue}</span>
+            )}
+            {partsText.teal}
+          </span>
+        </div>
+
+        <input
+          ref={innerRef}
+          onFocus={focusHandel}
+          className={fontClass}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlurHandler}
+        />
+      </div>
+      <div className={styles.container__close}>
+        <MetCircleIconBtn
+          styleIconBtn={styleIconBtn}
+          classNameIconBtn={classNameIconBtn}
+          onClick={onClick}
+          size={Size.SMALL}
+          icon={icon}
+          bgColor={bgColor}
+          defaultIconColor={defaultIconColor}
+          hoverColor={hoverColor}
+        />
+      </div>
+    </Container>
+  );
+};
