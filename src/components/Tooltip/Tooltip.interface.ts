c23f466f34d04ef493481866f42b8376d2c027d0
ivan <severinec7788@mail.ru>
2022-03-23 15:35:46 +0300

    fix: added props 'onHover' in .mdx file

--- src/components/Tooltip/Tooltip.interface.ts
+++ src/components/Tooltip/Tooltip.interface.ts
@@ -1,28 +1,28 @@
 import { MetCircleIconBtnProps } from "../CircleIconBtn";
 
 export interface MetTooltipProps extends MetCircleIconBtnProps {
-    /**
-     * Additional component styles
-     */
-    style?: object;
-    /**
-     * Additional component classes
-     */
-    className?: string;
-    /**
-     * Value
-     */
-    value: string;
-    /**
-     * Function for trigger event onHover for the tooltip
-     */
-     onHover: (e) => void;
-    /**
-     * Whether the floating tooltip card is visible or not
-     */
-    isOpen: boolean;
-    /**
-     * Font for component
-     */
-    fontClass?: string;
+  /**
+   * Additional component styles
+   */
+  style?: object;
+  /**
+   * Additional component classes
+   */
+  className?: string;
+  /**
+   * Value
+   */
+  value: string;
+  /**
+   * Function for trigger event onHover for the tooltip
+   */
+  onHover: (e) => void;
+  /**
+   * Whether the floating tooltip card is visible or not
+   */
+  isOpen: boolean;
+  /**
+   * Font for component
+   */
+  fontClass?: string;
 }
